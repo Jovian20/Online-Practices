0cd1b3ef1d1442a13926eb4bb3c7f2eb91ada223
Jovian20 <92246107+Jovian20@users.noreply.github.com>
2025-02-26 01:16:55 +0800

    Create The Minion Game.py

---
+++ Hackerrank/The Minion Game.py
@@ -0,0 +1,20 @@
+def minion_game(string):
+    # your code goes here
+    vowels = 'AEIOU' 
+    kevin = 0 
+    stuart = 0 
+    n = len(string)
+
+    for i in range(n):
+        if string[i] in vowels:
+            kevin += n - i  
+        else:
+            stuart += n - i 
+
+    if kevin > stuart:
+        print('Kevin', kevin)
+    elif stuart > kevin:
+        print('Stuart', stuart)
+    else:
+        print('Draw')
+
