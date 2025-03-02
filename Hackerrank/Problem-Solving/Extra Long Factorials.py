ff17b3919a97811c886adff5cc3700a1de6745f7
Jovian20 <92246107+Jovian20@users.noreply.github.com>
2025-03-02 22:02:06 +0800

    Create Extra Long Factorials.py

---
+++ Hackerrank/Problem-Solving/Extra Long Factorials.py
@@ -0,0 +1,17 @@
+import math
+import os
+import random
+import re
+import sys
+from math import factorial
+
+
+
+def extraLongFactorials(n):
+    # Write your code here
+    print(factorial(n))
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    extraLongFactorials(n)
